84aae02a2de633ff92e1492881f05d42131c51d1
SoyeonL <sylee932003@gmail.com>
2021-03-25 18:55:40 -0700

    update book_author table

--- migrations/m_20210326014421_book_author_table.php
+++ migrations/m_20210326014421_book_author_table.php
@@ -5,7 +5,7 @@ class m_20210326014421_book_author_table {
     $query = "CREATE TABLE book_author (
       title VARCHAR(255) PRIMARY KEY,
       author VARCHAR(255) PRIMARY KEY,
-      category_id INT NOT NULL SET DEFAULT 0,
+      category_id INT NOT NULL DEFAULT 0,
       FOREIGN KEY (title) REFERENCES book(title)
           ON DELETE CASCADE ON UPDATE CASCADE,
       FOREIGN KEY (author) REFERENCES book(author)
