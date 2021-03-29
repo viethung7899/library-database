7fc859952c06ed770f2284bee6b7a2acb8e2233d
viethung7899 <viethung07081999@gmail.com>
2021-03-28 17:35:35 -0700

    Add data about categories and books

---
+++ migrations/m_20210329002630_book_author_data.php
@@ -0,0 +1,18 @@
+<?php
+
+class m_20210329002630_book_author_data {
+  public function up(\PDO $pdo) {
+    $query = "INSERT INTO `book_author` VALUES 
+      ('Caveman', 'John Jacobs', 1),
+      ('If I Ran the Zoo', 'Dr.Seuss', 4),
+      ('Jnae Eyre', 'Charlotte Bronte', 4),
+      ('Night', 'Elie Wiesel', 1),
+      ('It', 'Stephen King', 2);";
+    $pdo->exec($query);
+  }
+
+  public function down(\PDO $pdo) {
+    $query = "DELETE FROM `book_author`";
+    $pdo->exec($query);
+  }
+}
