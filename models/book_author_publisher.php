c80e70636dccab1cf31b935f9643b18b5ac478c3
UsmanTung <usman-tung@hotmail.com>
2021-03-30 13:26:15 -0700

    getters and setters

--- models/book_author_publisher.php
+++ models/book_author_publisher.php
@@ -16,17 +16,70 @@ class Book_Author_Publisher extends Model
   public $year;
   public $quantity;
 
+  function set_title($title)
+  {
+    $this->title = $title;
+  }
+
+  function get_title()
+  {
+    return $this->title;
+  }
+
+  function set_author($author)
+  {
+    $this->author = $author;
+  }
+
+  function get_author()
+  {
+    return $this->author;
+  }
+
   function set_publisherId($pId)
   {
-    if (!publisher::checkForPublisher($pId)) {
-      //RETURN NO SUCH PUBLISHER OR ADD TO PUBLISHER TABLE BY ASKING FOR NAME SOMEHOW
-    } else {
-      $this->publisherId = $pId;
-    }
+    $this->publisherId = $pId;
   }
 
   function get_publisherId()
   {
     return $this->publisherId;
+  }
+
+  function set_categoryId($cId)
+  {
+    $this->categoryId = $cId;
+  }
+
+  function get_categoryId()
+  {
+    return $this->categoryId;
+  }
+
+  function set_year($years)
+  {
+    $this->year = $years;
+  }
+
+  function get_year()
+  {
+    return $this->year;
+  }
+
+  function set_quantity($num)
+  {
+    $this->quantity = $num;
+  }
+
+  function get_quantity()
+  {
+    return $this->quantity;
+  }
+
+  public static function addBook_Author_Publisher($data)
+  {
+    if (!Book_Author::checkForAuthor($data['author'] && Book_Author::checkForTitle($data['title']))) {
+      Book_Author::addBook_Author($data);
+    }
   }
 }
