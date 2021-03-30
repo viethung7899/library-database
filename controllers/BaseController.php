acf9317dfa75c22e924e470ac82bb7d7aa3e9597
viethung7899 <viethung07081999@gmail.com>
2021-03-29 19:32:09 -0700

    Remeber who is in

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\controllers;
+
 use app\core\Controller;
 use app\core\Response;
 use app\core\Request;
@@ -10,6 +11,11 @@ use app\models\User;
 class BaseController extends Controller {
   // Render the home page, can access without login
   public static function home() {
+    if (!self::isAuthenticated()) {
+      // Redirect to search
+      Response::redirect('/search');
+      return;
+    }
     $view = self::generateView('index', 'Home', 'withNavigation');
     $view->render();
   }
@@ -30,30 +36,8 @@ class BaseController extends Controller {
     $view = self::generateView('searchBook', 'Book search', 'withNavigation');
     // Resolve book search
     $body = Request::body();
-    if (isset($body)) {
-      $response = Book::searchBooksByName($body);
-    // If successful, print the result
-      if ($response->ok()) {
-        
-      }
     // Else, show the error on the form
     $view->render();
-  }
-
-  // Get books details
-  public static function book() {
-
-  }
-
-  // Any user can view thier settings (should it be abstract?)
-  public static function setting() {
-
-  }
-
-  public static function updatePassword() {
-    // Validate old password
-
-    // Change into new password
   }
 
   protected static function setSession(Response $response) {
@@ -63,7 +47,15 @@ class BaseController extends Controller {
     $session->set('level', $response->content['level']);
   }
 
-  protected static function isAuthenticated() {
+  public static function isAuthenticated() {
     return self::getSession()->get('id');
+  }
+
+  public static function protectedRoute() {
+    if (!self::isAuthenticated()) {
+      Response::redirect('/search');
+      return true;
+    }
+    return false;
   }
 }
--- views/index.php
+++ views/index.php
@@ -1,6 +1,8 @@
-<h1 class="my-5">Welcome to our database</h1>
-<div class="row">
-  <div class="col-md">Member</div>
-  <div class="col-md">Librarian</div>
-  <div class="col-md">Admin</div>
-</div>
+<?php
+
+use app\core\Application;
+
+$name = Application::getApp()->getSession()->get('name');
+
+?>
+<h1 class="my-5">Hello <?php echo $name ?></h1>
