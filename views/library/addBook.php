9c5879dd56a5dd4b8d998fb1df6215d669dd8a11
SoyeonL <sylee932003@gmail.com>
2021-03-30 20:54:53 -0700

    update addBook

--- views/library/addBook.php
+++ views/library/addBook.php
@@ -23,3 +23,47 @@ $yearField = $form->field('Year', 'year');
 <h1 class="my-5">New Books</h1>
 
 <?php $form->begin(); ?>
+
+<div class="mb-3">
+    <?php $isbnField->render() ?>
+</div>
+
+<div class="row">
+  <div class="col-md mb-3">
+    <?php $titleField->render() ?>
+  </div>
+  <div class="col-md mb-3">
+    <?php $authorField->render() ?>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md mb-3">
+    <?php $publisherIdField->render() ?>
+  </div>
+  <div class="col-md mb-3">
+    <?php $publisherNameField->render() ?>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md mb-3">
+    <?php $categoryIdField->render() ?>
+  </div>
+  <div class="col-md mb-3">
+    <?php $categoryNameField->render() ?>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md mb-3">
+    <?php $quantityField->render() ?>
+  </div>
+  <div class="col-md mb-3">
+    <?php $yearField->render() ?>
+  </div>
+</div>
+
+<button type="submit" class="my-2 btn btn-primary">Add</button>
+
+<?php $form->end(); ?>
