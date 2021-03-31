aab4269267126edc3d589bc8d6b482fc8e9b3daa
SoyeonL <sylee932003@gmail.com>
2021-03-30 20:47:08 -0700

    little change

--- views/library/addBook.php
+++ views/library/addBook.php
@@ -7,6 +7,15 @@ $model = $book ?? new Book();
 
 $form = new Form('/librarian/add', 'get', $model, $errors ?? []);
 
+$isbnField = $form->field('Isbn', 'isbn');
+$titleField = $form->field('Title', 'title');
+$authorField = $form->field('Author', 'author');
+$publisherIdField = $form->field('PublisherId', 'publisherId'); 
+$publisherNameField = $form->field('PublisherName', 'publisherName');
+$categoryIdField = $form->field('CategoryId', 'categoryId'); 
+$categoryNameField = $form->field('CategoryName', 'categoryName');
+$quantityField = $form->field('Quantity', 'quantity');
+$yearField = $form->field('Year', 'year');
 
 
 ?>
