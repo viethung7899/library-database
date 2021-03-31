1a28c22856a3c49d6b8f6d3a500b6836c4828a21
viethung7899 <viethung07081999@gmail.com>
2021-03-31 12:06:50 -0700

    Add pages

---
+++ views/profile.php
@@ -0,0 +1,34 @@
+<?php
+
+use app\components\form\Field;
+use app\components\form\Form;
+use app\models\User;
+
+$user = $body['user'] ?? new User();
+
+$form = new Form('/profile', Form::POST, $user, $errors ?? []);
+$nameField = $form->field('Name', 'name');
+$usernameField = $form->field('Username', 'username');
+$passwordField = $form->field('Password', 'password', true, Field::PASSWORD);
+
+?>
+
+<h1 class="my-5">Become new member</h1>
+
+<?php $form->begin(); ?>
+<div class="mb-3">
+  <?php $nameField->render() ?>
+</div>
+<div class="mb-3">
+  <?php $usernameField->render() ?>
+</div>
+<div class="mb-3">
+  <?php $passwordField->render() ?>
+</div>
+<button type="submit" class="my-2 btn btn-primary">Update</button>
+<a role="button" href="/search" class="my-2 btn btn-secondary">Cancel</a>
+<?php $form->end(); ?>
+
+<div class="mx-3">
+  <?php echo isset($body['success']) ? 'Success' : ''; ?>
+</div>
