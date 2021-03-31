f1514937b2482bcd40f0fbcc4652e9265f81585c
viethung7899 <viethung07081999@gmail.com>
2021-03-30 22:24:41 -0700

    Change definition

--- migrations/m_20210331050855_returned_column.php
+++ migrations/m_20210331050855_returned_column.php
@@ -4,9 +4,13 @@ class m_20210331050855_returned_column {
   public function up(\PDO $pdo) {
     $query = "ALTER TABLE borrow_record ADD COLUMN rerturned VARCHAR(1) DEFAULT 'N'";
     $pdo->exec($query);
+    $query = "ALTER TABLE borrow_record CHANGE COLUMN `borrowDate` `borrowDate` DATE DEFAULT CURRENT_DATE";
+    $pdo->exec($query);
   }
 
   public function down(\PDO $pdo) {
+    $query = "ALTER TABLE borrow_record CHANGE COLUMN `borrowDate` `borrowDate` DATE NOT NULL";
+    $pdo->exec($query);
     $query = "ALTER TABLE borrow_record DROP COLUMN rerturned";
     $pdo->exec($query);
   }
